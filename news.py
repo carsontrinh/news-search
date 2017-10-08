6a2bc303cb646f412e228a9bff4e56c77c52aeb2
Carson Trinh <trinh.carson@berkeley.edu>
2017-10-07 18:50:12 -0700

    Updated to be more general

--- news.py
+++ news.py
@@ -2,17 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-search_phrase = "asdfalsidhfalsdhfalsdkhfljhaeef wow"
-page = requests.get("https://news.google.com/news/search/section/q/" + search_phrase)
+def search(search_phrase):
+	page = requests.get("https://news.google.com/news/search/section/q/" + search_phrase)
 
-soup = BeautifulSoup(page.content, "lxml")
+	soup = BeautifulSoup(page.content, "lxml")
 
-i = 26
-for a in soup.find_all('a')[26:35]:
-	print("Link #" + str(i) + "\n")
-	print(str(a) + "\n")
-	i += 1
-
-href = soup.find_all('a')[26].get('href')
-# TODO If href is redirected to google.com, end the program
-print(href)
+	link_start = 26
+	'''
+	for a in soup.find_all('a')[26:35]:
+		print("Link #" + str(i) + "\n")
+		print(str(a) + "\n")
+		i += 1
+	'''
+	href = soup.find_all('a')[link_start].get('href')
+	# TODO If href is redirected to google.com, end the program
+	if href.startswith("https://www.google.com"):
+		return False
+	print(href)
+print(search("Donald Trump"))
